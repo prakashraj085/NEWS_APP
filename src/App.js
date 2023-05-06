5152269dad1076d40e8bc1577b5ddeb42777580b
prakashraj085 <107317063+prakashraj085@users.noreply.github.com>
2023-05-06 23:58:56 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,52 @@
+import React, { useState,useEffect } from 'react';
+import alanBtn from '@alan-ai/alan-sdk-web';
+
+import wordsToNumbers from 'words-to-numbers';
+
+import NewsCards from './components/NewsCards/NewsCards';
+import useStyles from './styles.js';
+
+
+const alanKey = "d1a87640917d4387dca581dda69392fb2e956eca572e1d8b807a3e2338fdd0dc/stage";
+
+const App = () => {
+    const [newsArticles, setNewsArticles] = useState([]);
+    const [activeArticle, setActiveArticle] = useState(-1);
+    const classes = useStyles()
+
+    useEffect(() => {
+        alanBtn({
+          key: alanKey,
+          onCommand: ({ command, articles, number }) => {
+            if (command === "newsHeadlines") {
+              setNewsArticles(articles);
+              setActiveArticle(-1);
+            } else if(command === 'highlight'){
+                setActiveArticle((prevActiveArticle) => prevActiveArticle+1);
+            }else if(command === 'open'){
+                const parsedNumber = number.length > 2 ? wordsToNumbers(number, {fuzzy:true}) : number;
+                const article = articles[parsedNumber-1];
+
+                if(parsedNumber>20){
+                    alanBtn().playText('Please try that again.')
+                }else if(article){
+                window.open(article.url,'_blank')
+                alanBtn.playText('Opening...')
+                }
+            }
+          }
+        })
+      }, [])
+      
+
+    return (
+         <div>
+            <div className={classes.logoContainer}>
+              <img src='https://i0.wp.com/synqqblog.wpcomstaging.com/wp-content/uploads/2019/11/preview.jpg?fit=1200%2C630&ssl=1' className={classes.alanLogo} alt="alan logo"/>
+            </div>
+            <NewsCards articles={newsArticles} activeArticle={activeArticle}/>
+         </div>
+    );
+}
+
+export default App;
